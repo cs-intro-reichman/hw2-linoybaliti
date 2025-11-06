8b74ea1b6deb5227e51ade41e285ddda90a6abd5
Linoy Baliti <linoy.baliti@post.runi.ac.il>
2025-11-06 22:10:06 +0200

    Cheers

--- Cheers.java
+++ Cheers.java
@@ -9,5 +9,28 @@ Great work boaz!
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+         String name = args[0];
+         int num = Integer.parseInt(args[1]);
+         int NameLength = name.length();
+
+          for (int i=0; i<NameLength; i++){ // לולאה שרצה ככמות האותיות בשם שנקלט
+                 char c =  name.charAt(i);
+                     if (c == 'A' || c == 'E' || c == 'F' || c == 'H' || c == 'I' || c == 'L' ||
+                  c == 'M' ||   c == 'N' ||   c == 'O' ||   c == 'R' || c == 'S' || c == 'X' ){
+                      System.out.println("Give" + " " + "me" + " " + "An" + " " + c + ":" + " " + c + "!" );
+                         }
+                         else {
+                      System.out.println("Give" + " " + "me" + " " + "A" + " " + c + ":" + " " + c + "!" );
+                         }
+                        
         }
+        System.out.println("what does that spell?" );
+
+                for (int j=0; j<num; j++){ // לולאה שרצה ככמות המספר שקלטנו
+                System.out.println(name + "!!!" );
+        }
+
+  }
+
+
 }
