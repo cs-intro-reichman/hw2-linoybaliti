d579331562d67f6c812a103d5c189acc05de5a25
Linoy Baliti <linoy.baliti@post.runi.ac.il>
2025-11-06 23:18:49 +0200

    CalcPi3

--- CalcPi.java
+++ CalcPi.java
@@ -8,30 +8,27 @@ public class CalcPi {
 	double sumMinus = 0;
 	
 	if (counter %2 == 0 ){
-		double x = 1.0; 
 		double y = 1.0; 
 		for (int i = 0; i<counter/2; i++){
-		sumPlus += x/y;
+		sumPlus += 1.0/y;
 		y = y+4.0;
 }
-		double xx = 1.0; 
+	
 		double yy = 3.0; 
 		for (int i = 0; i<counter/2; i++){
-		sumMinus += xx/yy;
+		sumMinus += 1.0/yy;
 		yy = yy+4.0;
 }
 	}
 else {
-		double x = 1.0; 
 		double y = 1.0; 
 		for (int i = 0; i<counter/2 +1; i++){
-		sumPlus += x/y;
+		sumPlus += 1.0/y;
 		y = y+4.0;
 }
-		double xx = 1.0; 
 		double yy = 3.0; 
 		for (int i = 0; i<counter/2; i++){
-		sumMinus += xx/yy;
+		sumMinus += 1.0/yy;
 		yy = yy+4.0;
 }
 
@@ -39,8 +36,8 @@ else {
 double FinalSum = (sumPlus - sumMinus)*4;
 //pi according to Java:
 //pi, approximated:
-System.out.println("pi according to Java:"+" "+pai);
-System.out.println("pi, approximated:"+"     "+FinalSum);
+System.out.println("pi according to Java:" +" "+pai);
+System.out.println("pi, approximated:" +"     "+FinalSum);
 
 	}
 }
